8b77af27ebe7fb4b5a7bada0142a0db81b20de27
sourashree banerjee <sourashree@sourashrees-MacBook-Air.local>
2025-03-02 10:22:01 +0530

    initial skeleton

---
+++ src/app/Components/Board/Board.jsx
@@ -0,0 +1,11 @@
+"use client"
+
+const Board = () =>{
+ return <div className="top-0 fixed right-0 bottom-0 left-20 bg-[#D9D9D9] -z-10">
+    <div className="text-white p-6 mt-24">
+        <div>
+        </div>
+    </div>
+ </div>
+}
+export default Board;
---
+++ src/app/Components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+"use client"
+
+const Navbar = () => {
+    return <div>
+        <div className="w-44 bg-[#ADAAB2] h-screen">
+            <div className="rounded-xl p-4">
+                <ul className="space-y-4 ">
+                    <li className="text-white text-xl py-10">
+                        Cycles
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+}
+
+export default Navbar;
