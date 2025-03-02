a20e8db4a0ab030158c2aa1491550691ada01c46
sourashree banerjee <sourashree03@gmail.com>
2025-03-02 10:22:01 +0530

    initial skeleton

---
+++ src/app/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,9 @@
+"use client"
+
+const Dashboard = () => {
+return <div>
+    <p>Dashboard</p>
+</div>
+}
+
+export default Dashboard;
---
+++ src/app/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () =>{
+    return <div className="mx-7 my-4 relative z-10">
+    <div className="h-16 bg-[#AA96AF] w-full rounded-lg">
+        <div className="h-full flex items-center">
+            <div className=" flex space-x-4">
+                <ul className="text-white p-2">
+                    <li>Board</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    </div>
+    
+}
+
+export default Header;
