739f74b46141b9d0d9b470d78573236ee522de68
sourashree banerjee <sourashree03@gmail.com>
2025-03-09 00:16:01 +0530

    fetching data from database and displaying them on the cards

--- src/app/Components/Navbar/Navbar.jsx
+++ src/app/Components/Navbar/Navbar.jsx
@@ -1,13 +1,36 @@
 "use client"
 
+import { supabase } from "@/utils/supabase/client";
+import { useEffect, useState } from "react";
+
 const Navbar = () => {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    async function fetchData() {
+        const { data, error } = await supabase
+            .from('cycles')
+            .select('*');
+
+        setData(data);
+        console.log(data);
+        if (error) {
+            throw error;
+        }
+    }
     return <div>
         <div className="w-52 bg-[#ADAAB2] h-screen z-10">
             <div className="rounded-xl p-4">
                 <ul className="space-y-4 ">
-                    <li className="text-white text-xl py-20">
-                        Cycles
-                    </li>
+                    {data && data.map((item) => (
+                        <li key={item.id} className="text-white text-xl py-20">
+                            {item.title}
+                        </li>
+                    ))}
+
                 </ul>
             </div>
         </div>
--- src/app/Components/Tasks/Tasks.jsx
+++ src/app/Components/Tasks/Tasks.jsx
@@ -1,52 +1,88 @@
+'use client'
+
+import { supabase } from "@/utils/supabase/client";
+import { useEffect, useState } from "react";
+
 const Tasks = () => {
-    return <div className="absolute z-20 top-24 mx-4">
-        <div className="flex gap-16">
-            {/* Not Started tasks */}
 
-            <div className="flex flex-col gap-2">
-                <div className="bg-white w-60 h-44 rounded-lg shadow-lg">
-                    <p className="text-gray-600 p-4"> Task 1</p>
-                </div>
-                <div className="bg-white w-60 h-44 rounded-lg shadow-lg">
-                    <p className="text-gray-600 p-4"> Task 2</p>
-                </div>
-                <div className="bg-white w-60 h-44 rounded-lg shadow-lg">
-                    <p className="text-gray-600 p-4"> Task 3</p>
-                </div>
-            </div>
-            {/* In Progress tasks */}
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
 
-            <div className="flex flex-col gap-2">
-                <div className="bg-white w-60 h-44 rounded-lg shadow-lg">
-                    <p className="text-gray-600 p-4"> Task 1</p>
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    async function fetchData() {
+        try {
+            setLoading(true);
+            const { data, error } = await supabase
+                .from('tasks')
+                .select('*')
+
+            if (error) {
+                throw error;
+            }
+            setData(data);
+            console.log(data);
+        } catch (error) {
+            console.error("eror fetching data", error);
+        } finally {
+            setLoading(false);
+        }
+
+    }
+
+    return (
+        <div>
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <div className="absolute z-20 top-24 mx-4">
+                    <div className="flex gap-16">
+                        {/* Not Started tasks */}
+                        <ul className="flex flex-col gap-2">
+                            {data && data.map((item) => (
+                                <div className="bg-white w-60 h-44 rounded-lg shadow-lg">    <li key={item.id} className="text-gray-600 p-4">{item.title}</li></div>
+                            ))}
+                        </ul>
+
+                        {/* In Progress tasks */}
+
+                        <div className="flex flex-col gap-2">
+                            <div className="bg-white w-60 h-44 rounded-lg shadow-lg">
+                                <p className="text-gray-600 p-4"> Task 1</p>
+                            </div>
+                            <div className="bg-white w-60 h-44 rounded-lg shadow-lg">
+                                <p className="text-gray-600 p-4"> Task 2</p>
+                            </div>
+                            <div className="bg-white w-60 h-44 rounded-lg shadow-lg">
+                                <p className="text-gray-600 p-4"> Task 3</p>
+                            </div>
+                        </div>
+                        {/* Under-Review tasks */}
+                        <div className="flex flex-col gap-2">
+                            <div className="bg-white w-60 h-44 rounded-lg shadow-lg">
+                                <p className="text-gray-600 p-4">Task 3</p>
+                            </div>
+                            <div className="bg-white w-60 h-44 rounded-lg shadow-lg">
+                                <p className="text-gray-600 p-4">Task 4</p>
+                            </div>
+                        </div>
+                        {/* Completed tasks */}
+                        <div className="flex flex-col gap-2">
+                            <div className="bg-white w-60 h-44 rounded-lg shadow-lg">
+                                <p className="text-gray-600 p-4">Task 1</p>
+                            </div>
+                        </div>
+                    </div>
+
+
+
                 </div>
-                <div className="bg-white w-60 h-44 rounded-lg shadow-lg">
-                    <p className="text-gray-600 p-4"> Task 2</p>
-                </div>
-                <div className="bg-white w-60 h-44 rounded-lg shadow-lg">
-                    <p className="text-gray-600 p-4"> Task 3</p>
-                </div>
-            </div>
-            {/* Under-Review tasks */}
-            <div className="flex flex-col gap-2">
-                <div className="bg-white w-60 h-44 rounded-lg shadow-lg">
-                    <p className="text-gray-600 p-4">Task 3</p>
-                </div>
-                <div className="bg-white w-60 h-44 rounded-lg shadow-lg">
-                    <p className="text-gray-600 p-4">Task 4</p>
-                </div>
-            </div>
-            {/* Completed tasks */}
-            <div className="flex flex-col gap-2">
-                <div className="bg-white w-60 h-44 rounded-lg shadow-lg">
-                    <p className="text-gray-600 p-4">Task 1</p>
-                </div>
-            </div>
+            )}
         </div>
 
-
-
-    </div>
+    )
 }
 
 export default Tasks;
