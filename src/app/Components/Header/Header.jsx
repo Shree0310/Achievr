eaa34100d7d8dff97936b035d450b89ef3d75692
sourashree banerjee <sourashree03@gmail.com>
2025-03-25 19:54:35 +0530

    Displaying the current month on the header

--- src/app/Components/Header/Header.jsx
+++ src/app/Components/Header/Header.jsx
@@ -1,10 +1,17 @@
 const Header = () =>{
+    const monthNames = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+
+    const currentMonth = monthNames[new Date().getMonth()];
+
     return <div className="mx-4 mt-4 relative z-10">
-    <div className=" bg-white rounded-lg">
+    <div className="bg-white rounded-lg">
         <div className="h-full flex items-center">
-            <div className=" flex space-x-4">
+            <div className="flex space-x-4">
                 <ul className="text-primary-500 font-semibold text-2xl px-6">
-                    <li>Month</li>
+                    <li>{currentMonth}</li>
                 </ul>
             </div>
         </div>
