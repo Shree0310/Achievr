e1a4d595936c0b7a748fef5211fbb62705e375d4
Shree0310 <sourashree03@gmail.com>
2025-07-17 10:43:34 +0530

    fetching notifications and displaying them on mian page

--- src/app/Components/Toast/Toast.jsx
+++ src/app/Components/Toast/Toast.jsx
@@ -1,18 +1,54 @@
 "use client";
 
+import { supabase } from "@/utils/supabase/client";
+import { useEffect, useState } from "react";
+
 const Toast = () => {
+  const [notifications, setNotifications] = useState([]);
+  useEffect(() => {
+    fetchNotifications();
+  }, []);
+
+  const fetchNotifications = async () => {
+    const { data, error } = await supabase
+      .from("notifications")
+      .select("id, title, event_type")
+      .order("event_time", { ascending: false });
+
+    if (error) {
+      console.log("error fetching the notifications");
+    }
+
+    try {
+      if (data && data.length > 0) {
+        setNotifications(data);
+        data.map((n) => {
+          console.log("Title:", n.title);
+          console.log("Event Type:", n.event_type);
+        });
+      }
+    } catch (error) {
+      console.log("notification not found");
+    }
+  };
   return (
     <>
       <div className="top-5 right-5 fixed z-30 flex-col gap-5">
         <div
           className={`bg-white border-l-green-700 border-l-4 rounded-lg p-4 shadow-lg
-                         border-neutral-400 min-w-80 max-w-96 min-h-32 max-h-44 cursor-pointer relative`}>
-          <div className="font-semibold text-lg">
-            New comment added to task 001
-          </div>
-          <div className="font-light text-neutral-500 text-sm">
-            Comment Title
-          </div>
+                      border-neutral-400 min-w-80 max-w-96 min-h-32 max-h-44 cursor-pointer relative`}>
+          {notifications.map((notification) => {
+            return (
+              <div key={notification.id}>
+                <div className="font-semibold text-lg">
+                  {notification.event_type}
+                </div>
+                <div className="font-light text-neutral-500 text-sm">
+                  {notification.title}
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
