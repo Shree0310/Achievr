21cd1fb44e151c9138791b4e3b5bb52ee15e61a6
Sourashree <sourashree03@gmail.com>
2025-12-03 12:04:59 +0530

    card two in features 2 section

--- src/app/Components/Hero/ProductDirection/ProductDirection.tsx
+++ src/app/Components/Hero/ProductDirection/ProductDirection.tsx
@@ -5,18 +5,19 @@ import Heading from "../Heading"
 import SubHeading from "../SubHeading"
 import { motion } from "motion/react";
 import { Card, SkeletonOne } from "./Skeletons/first";
+import SkeletonSecond from "./Skeletons/second";
 
 const ProductDirection = () => {
     return <Container className="mt-10 md:pt-4 ">
-        <div className="flex flex-col p-0 ml-36 mt-12 justify-start">
+        <div className="flex flex-col p-0  mt-12 justify-center items-center mx-auto">
                 <Heading className="">
                     Set the development flow
                 </Heading>
-                <div className="">
-                <SubHeading className="ml-8">
+                <div className="mx-auto">
+                <SubHeading className="mx-auto">
                     Align all the team members towrads the same goal. Plan, manage and track all product initiatives with achievr.        
                 </SubHeading>
-                <div className="grid grid-cols-1 mt-10 md:grid-cols-2 border-y border-neutral-500 dark:border-neutral-700 divide-x divide-neutral-200 dark:divide-neutral-700">
+                <div className="mx-auto grid grid-cols-1 mt-10 md:grid-cols-2 border-y border-neutral-500 dark:border-neutral-700 divide-x divide-neutral-200 dark:divide-neutral-700">
                     <div className="p-4 mx-16">
                         <h2 className="text-2xl font-semibold text-neutral-800 dark:text-neutral-300">Manage products end to end</h2>
                         <p className="text-neutral-600 dark:text-neutral-500 text-balance">Consolidate specs, milestones, tasks, and other documentation in one centralized location.</p>
@@ -26,9 +27,12 @@ const ProductDirection = () => {
                             </SkeletonOne>
                         </CardSkeleton>
                     </div>
-                    <div className="p-4 mx-24">
+                    <div className="p-4 mx-auto">
                         <h2 className="text-2xl font-semibold text-neutral-800 dark:text-neutral-300">Project updates</h2>
                         <p className="text-neutral-600 dark:text-neutral-500 text-balance">Communicate progress and project health with built-in project updates.</p>
+                         <CardSkeleton>
+                            <SkeletonSecond/>
+                        </CardSkeleton>
                     </div>
                 </div>
             </div>
