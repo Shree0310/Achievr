5755c99344fff0059f0b2c77571239c2c1463284
Sourashree <sourashree03@gmail.com>
2025-12-09 23:25:33 +0530

    Added another section

--- src/app/Components/Hero/Features-secondary/FeaturesSecondary.tsx
+++ src/app/Components/Hero/Features-secondary/FeaturesSecondary.tsx
@@ -4,6 +4,8 @@ import { CardSkeleton } from "../ProductDirection/ProductDirection";
 import SkeletonOne from "./Skeletons/first";
 import lineGraph from "../../../../utils/Images/line-graph.png"
 import { cn } from "@/lib/utils";
+import { LocationPinIcon } from "@/app/icons";
+import { BarChartIcon, DiamondIcon, PackageIcon } from "lucide-react";
 
 const FeaturesSecondary = () => {
     return <Container className="mt-36 md:pt-4 ">
@@ -31,6 +33,22 @@ const FeaturesSecondary = () => {
                         </CardSkeleton>
                     </div>
                 </div>
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-10 mx-16 p-4 mt-5 md:mt-10">
+            <Card icon={<LocationPinIcon className="size-4 flex items-center rounded-sm"/>} title="Initiatives" description="Coordinate strategic product efforts."/>
+            <Card icon={<PackageIcon className="size-4 flex items-center rounded-sm"/>} title="Cross-team projects" description="Collaborate across teams and departments."/>
+            <Card icon={<DiamondIcon className="size-4 flex items-center rounded-sm"/>} title="Milestones" description="Break projects down into concrete phases."/>
+            <Card icon={<PackageIcon className="size-4 flex items-center rounded-sm"/>} title="Progress insights" description="Track scope, velocity, and progress over time."/>                      
+        </div>
     </Container>
 }
 export default FeaturesSecondary;
+
+const Card = ({className, icon, title, description}:{className?: string, icon: React.ReactElement, title:string, description: string}) => {
+    return <div>
+                <div className={cn("flex items-center gap-2", className)}>
+                    {icon}
+                    <h3 className="font-bold text-sm text-neutral-600 dark:text-neutral-200 m-2">{title}</h3>
+                </div>
+                <p className="text-xs text-neutral-600 dark:text-neutral-500 px-2">{description}</p>
+    </div>
+}
